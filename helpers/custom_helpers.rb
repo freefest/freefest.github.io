791b560c37e283a48ccfd555022d78bc86ce095a
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2025-05-14 20:46:39 +0200

    Adds gig_duration helper to display start and end time of gigs

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,4 +1,18 @@
+require 'time'
+
 module CustomHelpers
+  DJ_SETS = ['GOSH DJ'].freeze
+
+  def gig_duration(timeslot, performer)
+    start_time = Time.parse(format_to_time(timeslot))
+    end_time = start_time + (30 * 60)
+    if DJ_SETS.include? performer
+      "#{start_time.strftime('%H:%M')}-"
+    else
+      "#{start_time.strftime('%H:%M')}-#{end_time.strftime('%H:%M')}"
+    end
+  end
+
   def format_to_time(timeslot)
     timeslot.sub('-', ':')
   end
--- spec/helpers/custom_helpers_spec.rb
+++ spec/helpers/custom_helpers_spec.rb
@@ -23,4 +23,26 @@ RSpec.describe CustomHelpers, type: :helper do
       end
     end
   end
+
+  describe '#gig_duration' do
+    it 'returns the correct interval when adding 30 minutes' do
+      expect(helper.gig_duration('18-00', 'a band')).to eq('18:00-18:30')
+      expect(helper.gig_duration('23-45', 'a band')).to eq('23:45-00:15')
+      expect(helper.gig_duration('00-00', 'a band')).to eq('00:00-00:30')
+    end
+
+    it 'handles invalid time formats gracefully' do
+      expect { helper.gig_duration('invalid', 'a band') }.to raise_error(ArgumentError)
+    end
+
+    it 'handles edge cases like 23:59 correctly' do
+      expect(helper.gig_duration('23-59', 'a band')).to eq('23:59-00:29')
+    end
+
+    context 'for the DJs specified' do
+      it 'displays open interval' do
+        expect(helper.gig_duration('23-59', 'GOSH DJ')).to eq('23:59-')
+      end
+    end
+  end
 end
